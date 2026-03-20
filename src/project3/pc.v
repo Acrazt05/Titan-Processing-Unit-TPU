64d327e04a502340198f873f5ec9915777393837
xavier (⁠ ⁠◜⁠‿⁠◝⁠ ⁠)⁠♡ <xavierbluesch@gmail.com>
2026-03-19 20:43:19 -0400

    Add files via upload

--- src/project3/pc.v
+++ src/project3/pc.v
@@ -1,21 +1,18 @@
-module pc (
-    input load,
-    input inc, 
-    input reset,
-    input clk,
-
+module PC (
+    input load, inc, reset, clk
     input [15:0] in,
     output reg [15:0] out
-);
+    );
 
-    always @(posedge clk or posedge reset) begin
-        if(reset) begin
-            out <= 16'b0;
-        end else if(load) begin
+    always@(posedge clk or posedge reset) begin
+        if(reset) begin //if reset is 1, counter is 0!
+          out <= 16'b0
+        end
+        else if(load) begin //else if load is 1, counter is input!
             out <= in;
-        end else if(inc) begin
+        end
+        else if(inc) begin //else if inc is 1, counter+1!
             out <= out + 1;
         end
     end
-    
 endmodule
