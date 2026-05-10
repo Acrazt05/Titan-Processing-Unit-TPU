d79d4c30230873cf44d3e497b6ba21fa6b78340a
EpicSpartan600 <vcasti5@live.spcollege.edu>
2026-05-09 21:31:42 -0400

    test

--- src/spi_memory.v
+++ src/spi_memory.v
@@ -24,102 +24,24 @@ module spi_memory (
     input ram_write,
 
     // Data returned to CPU
-    output reg [15:0] ram_data_out,
-    output reg [15:0] rom_data_out,
+    output [15:0] ram_data_out,
+    output [15:0] rom_data_out,
 
     // Signals CPU when both accesses finished
-    output reg data_ready,
+    output data_ready,
 
     // SPI pins
-    output reg mosi,
+    output mosi,
     input miso,
-    output reg cs,
+    output cs,
     output spi_clock
 );
 
-    //==========================================================
-    // Memory layout inside SPI RAM
-    //
-    // Each Hack word = 2 bytes
-    //
-    // ROM  : 0x0000 - 0x7FFF  (byte addresses)
-    // RAM  : 0x8000 - 0xFFFF
-    //==========================================================
-    parameter RAM_OFFSET = 16'h8000;
-
-    //==========================================================
-    // SPI clock generation
-    //==========================================================
-    //==========================================================
-    // Internal registers
-    //==========================================================
-
-    // SPI transmit register
-    reg [7:0] shift_reg;
-
-    // counts bits being transmitted
-    reg [2:0] bit_counter;
-
-    // current SPI byte address
-    reg [15:0] spi_address;
-
-    // stores bytes received from SPI
-    reg [7:0] byte0;
-    reg [7:0] byte1;
-
-    // FSM state register
-    reg [3:0] state;
-
-    reg dummy_bit;
-
-    reg [15:0] buffer_ram_data_out;
-    reg [15:0] buffer_rom_data_out;
-
-    //==========================================================
-    // SPI command values
-    //==========================================================
-    localparam SPI_READ  = 8'h03;
-    localparam SPI_WRITE = 8'h02;
-
-    //==========================================================
-    // State Machine
-    //==========================================================
-    localparam IDLE            = 0;
-
-    localparam ROM_CMD         = 1;
-    localparam ROM_ADDR_H      = 2;
-    localparam ROM_ADDR_L      = 3;
-    localparam ROM_DATA_0      = 4;
-    localparam ROM_DATA_1      = 5;
-
-    localparam RAM_CMD         = 6;
-    localparam RAM_ADDR_H      = 7;
-    localparam RAM_ADDR_L      = 8;
-    localparam RAM_DATA_0      = 9;
-    localparam RAM_DATA_1      = 10;
-
-    localparam RAM_WRITE_DATA0 = 11;
-    localparam RAM_WRITE_DATA1 = 12;
-
-    localparam DONE            = 13;
-
-    reg spi_phase;
-
-    reg [1:0] counter;
-    wire clk_en = (counter == 2'd3);
-    assign spi_clock = clk_en;
-
-    always @(posedge clk or posedge reset) begin
-        if(reset || data_ready) begin //TODO: maybe don't need data_ready here
-            spi_phase <= 0;
-            state <= IDLE;
-            cs <= 1;
-            data_ready <= 0;
-            mosi <= 0;
-            dummy_bit <= 1'b0;
-            counter <= 2'd0;
-
-        end 
-    end
-
+    assign ram_data_out = 16'b0;
+    assign rom_data_out = 16'b0;
+    assign data_ready = 1'b0;
+    assign mosi = 1'b0;
+    assign cs = 1'b0;
+    assign spi_clock = 1'b0;
+ 
 endmodule
